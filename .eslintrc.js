0cf2333aef72f9a9f828e43124ab32ea45dfe503
Petr Brovkin <bropet@mail.ru>
2020-08-20 11:59:30 +0300

    initial test

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+      'import',
+      '@typescript-eslint'
+  ],
+  env: {
+      browser: true,
+      es6: true,
+      node: true
+  },
+  extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+      // 'no-unused-vars': ['error', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }],
+      'indent': ["error", 2],
+      'import/no-unresolved': 'off',
+      'import/no-dynamic-require': 'off',
+      'no-await-in-loop': 'off',
+      'no-unused-expressions': 'off',
+      'no-multi-spaces': 'off',
+      'key-spacing': 'off',
+      'global-require': 'off',
+      '@typescript-eslint/member-delimiter-style': [1, {
+          multiline: {
+            delimiter: 'semi',
+            requireLast: true
+          },
+          singleline: {
+            delimiter: 'comma',
+            requireLast: false
+          }
+        }],
+      'no-warning-comments': ['warn', {
+          "terms": [''],
+          "location": "start"
+      }]
+  },
+  settings: {
+      '@typescript-eslint/parser': ['.ts'],
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true
+        }
+      }
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import './css/style.css';
+
+const a = document.querySelector('.main');
+// a?.textContent = 'ewlew,dl'
+// console.log(a);
+
+console.log("hello world");
+
+function name() {
+  const a: string = '12';
+}
