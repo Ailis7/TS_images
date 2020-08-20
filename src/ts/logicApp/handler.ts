27ed4333ae447564d2590f02724b241e6827ebee
Petr Brovkin <bropet@mail.ru>
2020-08-21 00:31:10 +0300

    fix and optimize

--- src/ts/logicApp/handler.ts
+++ src/ts/logicApp/handler.ts
@@ -3,9 +3,9 @@ import { of } from 'rxjs'
 import { catchError } from 'rxjs/operators'
 import { picsumType } from './types'
 
-const main = document.querySelector('.main')!;
+const main = document.querySelector('.main');
 
-const obj$ = ajax.getJSON(`https://picsum.photos/v2/list?page=2&limit=20`)
+const observer$ = ajax.getJSON(`https://picsum.photos/v2/list?page=2&limit=20`)
   .pipe(
     catchError(error => {
       console.log('error: ', error);
@@ -13,30 +13,30 @@ const obj$ = ajax.getJSON(`https://picsum.photos/v2/list?page=2&limit=20`)
     })
   )
 
-obj$.subscribe((v) => {
-  (v as picsumType[]).forEach((element) => {
-    function handler(elem: picsumType) {
-      const imageContainer = document.createElement('div');
-      imageContainer.classList.add('image_container');
-      imageContainer.innerHTML = `
-        <div class="show"></div>
-        <img src=${elem.download_url} alt="image" class="image">
-        `;
-      main.append(imageContainer);
-    }
+observer$.subscribe((value) => {
+  const handler = (elem: picsumType) => {
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('image_container');
+    imageContainer.innerHTML = `
+      <div class="show"></div>
+      <img src=${elem.download_url} alt="image" class="image">
+      `;
+    main?.append(imageContainer);
+  }
+  (value as picsumType[]).forEach((element) => {
     handler(element)
   });
-  
+
   const imagesArr = Array.from(document.querySelectorAll('.image_container'));
   
-  main.addEventListener('click', (event) => {
+  main?.addEventListener('click', (event) => {
     const target = <HTMLElement>event.target;
-    const show = (target.classList.contains('show')) ? target : target.previousElementSibling!;
-    const parrent = target.closest('.image_container')!;
+    const show = (target.classList.contains('show')) ? target : target.previousElementSibling as Element;
+    const parrent = target.closest('.image_container');
     show.classList.toggle('active');
-    if (show.textContent === '') {
-      const index = imagesArr.indexOf(parrent);
-      show.textContent = v[index].author
+    if (show?.textContent === '') {
+      const index = imagesArr.indexOf(parrent as Element);
+      show.textContent = value[index].author
     } else {
       show.textContent = '';
     }
